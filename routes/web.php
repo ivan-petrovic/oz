15082c3520aff51934c5831e02ffa3976ab5aa1b
Ivan Petrovic <mangan77@gmail.com>
2017-11-22 14:30:04 +0100

    Testing heroku database

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 Route::get('/', function() {
-    return view('welcome');
+    // return view('welcome');
+    return getenv("DATABASE_URL");
 });
 
 Route::get('/firma', 'FirmasController@index')->name('lista_firmi');
